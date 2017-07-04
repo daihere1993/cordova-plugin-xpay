a2b66f9be277327bfb3d8201b99f6b16bda53072
daihere <daihere1993@gmail.com>
2017-07-04 16:17:03 +0800

    Add alipay for android

--- src/android/Xpay.java
+++ src/android/Xpay.java
@@ -11,6 +11,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Map;
+
+import android.os.Message;
+import android.text.TextUtils;
+import android.widget.Toast;
+
 // wechat sdk
 import com.tencent.mm.opensdk.modelpay.PayReq;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
@@ -23,6 +29,66 @@ public class Xpay extends CordovaPlugin {
     public static final String TAG = "Cordova.Pligun.Xpay";
     public static IWXAPI wxAPI;
     public static CallbackContext currentCallbackContext;
+
+    private static final int SDK_PAY_FLAG = 1;
+
+    protected  boolean aliPayment(String orderInfo, CallbackContext callbackContext) {
+        cordova.getTheadPool().execute(new Runable() {
+           @Override
+            public void run() {
+               PayTask alipay = new PayTask(cordova.getActivity());
+               Map<String, String>result = alipay.payV2(orderInfo, true);
+               Log("msp", result.toString());
+
+               Message msg = new Message();
+               msg.what = SDK_PAY_FLAG;
+               msg.obj = result;
+               mHandler.sendMessage(msg);
+
+               PayResult payResult = new PayResult(result);
+               String resultInfo = payResult.getResult(); //同步返回需要验证的信息
+               String resultStatus = payResult.getResultStatus();
+               // 判断resultStatus为9000则代表支付成功
+               if (TextUtils.equals(resultStatus, "9000")) {
+                   // 该笔订单是否真实支付成功，需要依赖服务端的异步通知
+                   callbackContext.success(new JSONObject(result));
+               } else {
+                   // 该笔订单真实的支付结果，需要依赖服务端的异步通知
+                   callbackContext.error(new JSONObject(result));
+               }
+           }
+        });
+    }
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @SuppressWarnings("unused")
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SDK_PAY_FLAG: {
+                    @SuppressWarnings("unchecked")
+                    PayResult payResult = new PayResult((Map<String, String>) msg.obj);
+                    /**
+                     对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
+                     */
+                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+                    String resultStatus = payResult.getResultStatus();
+                    // 判断resultStatus 为9000则代表支付成功
+                    // 判断resultStatus 为9000则代表支付成功
+                    if (TextUtils.equals(resultStatus, "9000")) {
+                        // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
+                        Toast.makeText(cordova.getActivity(), "支付成功" + resultStatus, Toast.LENGTH_SHORT);
+                    } else {
+                        // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+                        Toast.makeText(cordova.getActivity(), "支付失败" + resultStatus, Toast.LENGTH_SHORT);
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+    };
 
     protected boolean wechatPayment(CordovaArgs args, CallbackContext callbackContext) {
         final JSONObject params;
@@ -71,6 +137,10 @@ public class Xpay extends CordovaPlugin {
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("wechatPayment")) {
             wechatPayment(args, callbackContext);
+            return true;
+        } else if (action.equals("aliPayment")) {
+            String orderInfo = args.getString(0);
+            aliPayment(orderInfo, callbackContext);
             return true;
         }
         return false;
